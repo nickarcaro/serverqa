8d533e6f9ac3a5015b271c752f4377a72265c582
YuiZh0u <yui.zhou97@gmail.com>
2020-10-12 02:38:22 -0300

    add cors module

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+var cors = require('cors');
 const bodyParser = require("body-parser");
 const { API_VERSION } = require("./config");
 
 const app = express();
+app.use(cors());
 
 // carga de rutas
 const userRoutes = require("./routes/user");
@@ -18,10 +20,10 @@ app.use(`/api/${API_VERSION}`, userRoutes);
 
 module.exports = app;
 
-/* comentarios respecto al funcioamiento del servidor 
+/* comentarios respecto al funcioamiento del servidor
 1. se crea el modelo en la carpeta models (donde se conecta a mongodb)
 2. se crea el controllador, que realiza las operaciónes ligadas al modelo (antes debe importar el modelo)
-   por ej: modelo usuario, el controlador hace registro , login , etc.  
+   por ej: modelo usuario, el controlador hace registro , login , etc.
 3. se crea la ruta en carpeta routes, importando el controllador  ( la /ruta de la api)
-4. se importa en app, para ser usados en la api 
+4. se importa en app, para ser usados en la api
 */
